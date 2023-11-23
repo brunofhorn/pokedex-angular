278dfc3d9780e6b9fae6af71a14df8dfa0c08775
brunofhorn <b.fernandeshorn@gmail.com>
2023-11-23 00:45:42 -0300

    feat: :tada: add pokeapi

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,8 +4,6 @@ import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 
-
-
 @NgModule({
   declarations: [
     PokeHeaderComponent,
